02e85f75be34f12056d0624f96f295c43523ae28
Nishonovich <nameshavkat@mail.ru>
2022-09-09 18:31:16 +0500

    Initial Commit

---
+++ src/OnlineFoodOrdering-WPF/OnlineFoodOrdering.Domain/Constants/DBConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineFoodOrdering.Domain.Constants
+{
+    public class DBConstants
+    {
+        public const string connectionString = "Host=localhost; Port=5432; Database=Online-Food-Ordering; " +
+            "User Id = Postgres; Password=admin;";
+
+
+    }
+}
