01fdd519105189112eb1ea245f6a5efbf3ea83c4
Temur <ubaydullayev117@gmail.com>
2022-09-10 17:56:54 +0500

    DbContext update

--- src/OnlineFoodOrdering-WPF/OnlineFoodOrdering.Data/DbContexts/AppDbContext.cs
+++ src/OnlineFoodOrdering-WPF/OnlineFoodOrdering.Data/DbContexts/AppDbContext.cs
@@ -14,6 +14,8 @@ namespace OnlineFoodOrdering.Data.DbContexts
         public virtual DbSet<Customer> Customers { get; set; } = null!;
         public virtual DbSet<Employee> Employees { get; set; } = null!;
         public virtual DbSet<Payment> Payments { get; set; } = null!;
+        public virtual DbSet<Order> Orders { get; set; } = null!;
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
