100dccb7ce7ccc72e010c7315e0b9ca6a8fdace0
Temur <ubaydullayev117@gmail.com>
2022-09-10 17:04:47 +0500

    Order and OrderDetails classes added

---
+++ src/OnlineFoodOrdering-WPF/OnlineFoodOrdering.Domain/Entities/Order.cs
@@ -0,0 +1,24 @@
+﻿using OnlineFoodOrdering.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineFoodOrdering.Domain.Entities
+{
+    public class Order : Auditable
+    {
+        [Required]
+        public long CustomerId { get; set; }
+        [Required]
+        public float TotalAmount { get; set; }
+        [Required]
+        public bool OrderStatus { get; set; }
+        [Required]
+        public long EmployeeId { get; set; }
+        [Required]
+        public string Address { get; set; } = string.Empty;
+    }
+}
