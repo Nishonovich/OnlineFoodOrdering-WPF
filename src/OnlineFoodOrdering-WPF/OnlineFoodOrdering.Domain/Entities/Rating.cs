696e22d914d6d2629788c86d7c8675dde8f4d906
Nishonovich <nameshavkat@mail.ru>
2022-09-10 18:57:54 +0500

    FullAll

--- src/OnlineFoodOrdering-WPF/OnlineFoodOrdering.Domain/Entities/Rating.cs
+++ src/OnlineFoodOrdering-WPF/OnlineFoodOrdering.Domain/Entities/Rating.cs
@@ -1,6 +1,7 @@
 ﻿using OnlineFoodOrdering.Domain.Common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,17 @@ namespace OnlineFoodOrdering.Domain.Entities
 {
     public class Rating : Auditable
     {
-        public int FoodId { get; set; }
+        [Required]
+        public long FoodId { get; set; }
+        public virtual Food Food { get; set; }
+    
         public short Score { get; set; }
+        
         public string Comment { get; set; } = String.Empty;
+
+        public Rating()
+        {
+            Food = new Food();
+        }
     }
 }
