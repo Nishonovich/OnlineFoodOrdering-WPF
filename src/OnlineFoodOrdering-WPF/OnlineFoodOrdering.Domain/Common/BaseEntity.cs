b53bd3b7f7312d36144211a63399c37a2b358bf5
Nishonovich <nameshavkat@mail.ru>
2022-09-10 18:31:40 +0500

    Food FoodType Update

--- src/OnlineFoodOrdering-WPF/OnlineFoodOrdering.Domain/Common/BaseEntity.cs
+++ src/OnlineFoodOrdering-WPF/OnlineFoodOrdering.Domain/Common/BaseEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace OnlineFoodOrdering.Domain.Common
 {
     public class BaseEntity
     {
+        [Required]
         public long Id { get; set; }
     }
 }
