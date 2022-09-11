52a5cbc2414eb4f148499d00cf94a96321a4538e
shahriyorbozorov <shahriyorbozorov12@gmail.com>
2022-09-11 14:30:07 +0500

    Added in Data/Common

---
+++ src/OnlineFoodOrdering-WPF/OnlineFoodOrdering.Data/Common/Interfaces/IUpdateable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineFoodOrdering.Data.Common.Interfaces
+{
+    public interface IUpdateable<T> where T : class
+    {
+        public Task<T> UpdateAsync(long id, T entity);
+    }
+
+}
