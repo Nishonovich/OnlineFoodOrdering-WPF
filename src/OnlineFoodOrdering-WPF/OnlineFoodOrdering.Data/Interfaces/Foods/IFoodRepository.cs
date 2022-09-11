3644e20bc647b61cb7948301d1bb9e35f730ef3b
shahriyorbozorov <shahriyorbozorov12@gmail.com>
2022-09-11 16:43:45 +0500

    IRepository added interfaces

---
+++ src/OnlineFoodOrdering-WPF/OnlineFoodOrdering.Data/Interfaces/Foods/IFoodRepository.cs
@@ -0,0 +1,13 @@
+﻿using OnlineFoodOrdering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineFoodOrdering.Data.Interfaces.Foods
+{
+    public interface IFoodRepository : IGenericRepository<Food>
+    {
+    }
+}
