9242fd4ab2e2409d6c645cd5fb4e943d6dfb044f
Temur <ubaydullayev117@gmail.com>
2022-12-16 18:05:00 +0500

    update

--- src/OnlineFoodOrdering-WPF/OnlineFoodOrdering.Domain/Constants/DBConstants.cs
+++ src/OnlineFoodOrdering-WPF/OnlineFoodOrdering.Domain/Constants/DBConstants.cs
@@ -9,6 +9,6 @@ namespace OnlineFoodOrdering.Domain.Constants
     public class DBConstants
     {
         public const string connectionString = "Host=localhost; Port=5432; Database=Online-Food-Ordering; " +
-            "User Id = Postgres; Password=admin;";
+            "User Id = Postgres; Password=temur;";
     }
 }
